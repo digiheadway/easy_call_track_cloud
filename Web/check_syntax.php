f3f3184e51b2e8463aa015c4b6259c9d5c941d0b
digiheadway <digiheadway@gmail.com>
2026-01-02 17:50:22 +0530

    Remove temp/debug scripts from Web folder

--- Web/check_syntax.php
+++
@@ -1,5 +0,0 @@
-<?php
-$file = 'api/sync_app.php';
-$content = file_get_contents($file);
-$result = shell_exec("php -l " . escapeshellarg($file));
-echo json_encode(["status" => true, "output" => $result]);
